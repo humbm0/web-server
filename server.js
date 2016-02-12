d55cc8a55314db2a43b895930295fb9fb41f2de5
Max Humberstone <maxhumberstonedesigns@gmail.com>
2016-02-12 21:04:41 +0000

    Capitalised about us

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(middleware.logger);
 // });
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-  res.send('about us');
+  res.send('About us!');
 });
 
 app.use(express.static(__dirname + '/public'));
